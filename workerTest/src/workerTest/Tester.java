4f5c4e8b1463842151e21a90bade9d4ed3f4f27a
zfeng <zfeng@172.17.30.119>
2018-12-31 14:35:53 -0500

    Functional works

--- workerTest/src/workerTest/Tester.java
+++ workerTest/src/workerTest/Tester.java
@@ -6,10 +6,12 @@ import java.util.LinkedList;
 public class Tester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Queue<Order> order=new LinkedList<Order>();
+		Queue<Order> orderQueue=new LinkedList<Order>();
 		for (int i=1;i<11;i++) {
-			Worker wk=new Worker();
-			Thread thr=new Thread(wk,"order"+i);
+			Order ord=new Order(i);
+			Worker wk=new Worker(orderQueue,ord);
+			Thread thr=new Thread(wk);
+			System.out.println("The New order "+ord.getNumber()+" has been generated!");
 			thr.start();
 		}
 	}
--- workerTest/src/workerTest/Worker.java
+++ workerTest/src/workerTest/Worker.java
@@ -2,13 +2,47 @@ package workerTest;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Random;
 
 public class Worker implements Runnable {
-	Queue<Order> order=new LinkedList<Order>();
+	Queue<Order> orderQueue;
+	Order order;
+
+	public Worker(Queue<Order> queue, Order order) {
+		this.orderQueue = queue;
+		this.order = order;
+	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		synchronized (orderQueue) {
+			while (orderQueue.size() == 5) {
+				try {
+					orderQueue.wait();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			orderQueue.add(order);
+			System.out.println("The order " + order.getNumber() + " is adding to the Queue.");
+		}
+		try {
+			Random r = new Random();
+			int workingTime = r.nextInt(2000);
+			System.out.println("The order " + order.getNumber() + " is working!");
+			Thread.sleep(workingTime + 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		synchronized (orderQueue) {
+			System.out.println("The order " + order.getNumber() + " is done!");
+			order.changeState();
+			orderQueue.remove(order);
+			orderQueue.notifyAll();
+		}
+
 	}
 }
